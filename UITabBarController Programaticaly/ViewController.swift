f4de597727243f8e2178afc80f0d45f1c35b60d7
masato <masato@m-mbp.local>
2018-10-08 13:28:36 +0800

    initial Commit

--- UITabBarController Programaticaly/ViewController.swift
+++ UITabBarController Programaticaly/ViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITabBarControllerDelegate {
+
+    // Declare TabBarController Instance
+    var myTabBarController: UITabBarController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // call Function
+        createTabBarController()
     }
 
 
+//************************************************************
+//  set Function
+    func createTabBarController() {
+
+        // set UITabBarController Instance
+        myTabBarController = UITabBarController()
+
+        // set delegate
+        myTabBarController.delegate = self
+
+        // set tabBar style
+        myTabBarController.tabBar.barStyle = .black
+
+
+
+        // First Screen
+        let firstViewController = UIViewController()
+
+        // First Screen Title
+        firstViewController.title = "First"
+
+        // set First Screen red
+        firstViewController.view.backgroundColor = .red
+
+
+
+        // Second Screen
+        let secondViewController = UIViewController()
+
+        // Second Screen
+        secondViewController.title = "Second"
+
+        // set Second Screen yellow
+        secondViewController.view.backgroundColor = .yellow
+
+
+
+
+        // set First and Second Elements to TabBar
+        myTabBarController.viewControllers = [firstViewController,secondViewController]
+
+        // add myTabBarController on Subview
+        self.view.addSubview(myTabBarController.view)
+    }
+
+
+//************************************************************
+
+
+        // get TabBar Index Number in Console
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController){
+
+        print("Test present Content Index \(tabBarController.selectedIndex)")
+
+
+    }
 }
 
